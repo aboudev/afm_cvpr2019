cf5cf6446c91d95696f7736b43bc1e791ef1c785
Lingjie Zhu <lingjie.zhu@nlpr.ia.ac.cn>
2019-07-17 04:47:47 +0000

    inference with the atrous configuration

---
+++ inference.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Inference on with the atrous configuration.
+#
+
+python test.py \
+  --config-file experiments/afm_atrous.yaml \
+  --gpu 0 \
+  TEST.OUTPUT_MODE save \
+  DATASETS.TEST "('ortho',)" \
+
